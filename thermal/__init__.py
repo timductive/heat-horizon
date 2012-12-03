c0e796cef7ce51a6a529d199a056e9de88da75b5
Dan Radez <dradez@redhat.com>
2012-12-03 10:51:23 -0500

    updating catalogue configs for heats move to the openstack github org

--- thermal/__init__.py
+++ thermal/__init__.py
@@ -1,10 +1,11 @@
+# TODO: move this to a more configurable location
 CATALOGUES = {
   ## Valid types are github and aws
   ## template name will be appended to base to download the template contents
   'github-heatapi-template': {
     'type': 'github',
-    'feed': 'https://api.github.com/repos/heat-api/heat/contents/templates',
-    'base': 'https://raw.github.com/heat-api/heat/master/templates/',
+    'feed': 'https://api.github.com/repos/openstack/heat/contents/templates',
+    'base': 'https://raw.github.com/openstack/heat/master/templates/',
   },
   'aws-cloudformation-templates-us-east-1': {
     'type': 'aws',
